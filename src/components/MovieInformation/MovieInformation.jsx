1034f3d3316344e7960bf10c69d71b93212c3d60
Mohamed-Oday <160322375+Mohamed-Oday@users.noreply.github.com>
2024-11-08 14:54:42 +0100

    file and folder structure

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MovieInformation = () => {
+    console.log("Movie Information");
+  
+    return (
+    <div>
+      Movie information
+    </div>
+  );
+};
+
+export default MovieInformation;
